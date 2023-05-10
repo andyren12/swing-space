258ffae590462ca42e96d3a6af99b28e0c678d0e
Andy Ren <andyren33@gmail.com>
2023-05-10 14:16:41 -0700

    Add login and signup buttons to home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,26 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Home</main>;
+  const { push } = useRouter();
+  return (
+    <main className={styles.main}>
+      <button
+        onClick={() => {
+          push("/login");
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          push("/signup");
+        }}
+      >
+        Sign up
+      </button>
+    </main>
+  );
 }
