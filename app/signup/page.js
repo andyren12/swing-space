f47ee43a5643de643c705bb84bcf233e3837238a
Andy Ren <andyren33@gmail.com>
2023-05-10 14:02:37 -0700

    Create signup page

---
+++ app/signup/page.js
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const { push } = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await axios
+        .post("http://localhost:3001/api/register", {
+          name,
+          email,
+          phone,
+          password,
+        })
+        .then((res) => {
+          if (res.data) {
+            console.log(res.data);
+            push("/");
+          } else {
+            console.log("bad");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "200px",
+          gap: "20px",
+        }}
+        action="POST"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="name"
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="email"
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+          placeholder="phone"
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="password"
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
