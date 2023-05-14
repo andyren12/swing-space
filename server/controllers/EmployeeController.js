7e6017f50184198c60a6bd964c8854e352a96618
Andy Ren <andyren33@gmail.com>
2023-05-13 18:56:39 -0700

    Remove unused callback parameter

--- server/controllers/EmployeeController.js
+++ server/controllers/EmployeeController.js
@@ -1,96 +1,94 @@
 const Employee = require("../models/Employee");
 
-const index = (req, res, next) => {
-  Employee.find()
-    .then((response) => {
-      res.json({
-        response,
-      });
-    })
-    .catch((error) => {
-      res.json({
-        message: "An error has occurred!",
-      });
+const index = async (req, res) => {
+  try {
+    const employee = Employee.find();
+    res.json({
+      employee,
     });
-};
-
-const show = (req, res, next) => {
-  let employeeID = req.body.employeeID;
-  Employee.findById(employeeID)
-    .then((response) => {
-      res.json({
-        response,
-      });
-    })
-    .catch((error) => {
-      res.json({
-        message: "An error has occurred!",
-      });
+  } catch (err) {
+    res.json({
+      message: "An error has occurred!",
     });
-};
-
-const store = (req, res, next) => {
-  let employee = new Employee({
-    name: req.body.name,
-    designation: req.body.designation,
-    email: req.body.email,
-    phone: req.body.phone,
-    age: req.body.age,
-  });
-
-  if (req.file) {
-    employee.avatar = req.file.path;
   }
-  employee
-    .save()
-    .then((response) => {
+};
+
+const show = async (req, res) => {
+  try {
+    const employeeID = req.body.employeeID;
+    const employee = await Employee.findById(employeeID);
+    res.json({
+      employee,
+    });
+  } catch (err) {
+    res.json({
+      message: "An error has occurred",
+    });
+  }
+};
+
+const store = async (req, res) => {
+  try {
+    let employee = new Employee({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      designation: req.body.designation,
+      email: req.body.email,
+      phone: req.body.phone,
+      age: req.body.age,
+    });
+
+    if (req.file) {
+      employee.avatar = req.file.path;
+    }
+    employee = await employee.save();
+    if (employee) {
       res.json({
         message: "Employee added successfully!",
       });
-    })
-    .catch((error) => {
-      res.json({
-        message: "An error hass occurred!",
-      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.json({
+      message: "An error has occurred!",
     });
+  }
 };
 
-const update = (req, res, next) => {
-  let employeeID = req.body.employeeID;
-  let updatedData = {
-    name: req.body.name,
-    designation: req.body.desgination,
-    email: req.body.email,
-    phone: req.body.phone,
-    age: req.body.age,
-  };
+const update = async (req, res) => {
+  try {
+    const employeeID = req.body.employeeID;
+    let updatedData = {
+      name: req.body.name,
+      designation: req.body.desgination,
+      email: req.body.email,
+      phone: req.body.phone,
+      age: req.body.age,
+    };
 
-  Employee.findByIdAndUpdate(employeeID, { $set: updatedData })
-    .then(() => {
-      res.json({
-        message: "Employee updated successfully!",
-      });
-    })
-    .catch((error) => {
-      res.json({
-        message: "An error has occurred!",
-      });
+    await Employee.findByIdAndUpdate(employeeID, { $set: updatedData });
+    res.json({
+      message: "Employee updated successfully!",
     });
+  } catch (err) {
+    res.json({
+      message: "An error has occurred!",
+    });
+  }
 };
 
-const remove = (req, res, next) => {
-  let employeeID = req.body.employeeID;
-  Employee.findByIdAndRemove(employeeID)
-    .then(() => {
-      res.json({
-        message: "Employee deleted successfully!",
-      });
-    })
-    .catch((error) => {
-      res.json({
-        message: "An error has occurred!",
-      });
+const remove = async (req, res) => {
+  try {
+    const employeeID = req.body.employeeID;
+    await Employee.findByIdAndRemove(employeeID);
+    res.json({
+      message: "Employee deleted successfully!",
     });
+  } catch (err) {
+    res.json({
+      message: "An error has occurred!",
+    });
+  }
 };
 
 module.exports = {
