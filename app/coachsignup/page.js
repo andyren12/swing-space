7a1b3f77e295e3c50aec0b2e1344eea2ed77d862
Andy Ren <andyren33@gmail.com>
2023-05-14 20:44:26 -0700

    Add coach signup page

---
+++ app/coachsignup/page.js
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function CoachSignup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userExists, setUserExists] = useState(false);
+  const { push } = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const user = await axios.post("http://localhost:3001/api/register", {
+        firstName,
+        lastName,
+        email,
+        password,
+        role: "coach",
+      });
+      if (user.data.message === "User already exists") {
+        setUserExists(true);
+      } else {
+        console.log(user.data);
+        push("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "200px",
+          gap: "20px",
+        }}
+        action="POST"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+          placeholder="first name"
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+          placeholder="last name"
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="email"
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="password"
+        />
+        <input type="submit" />
+      </form>
+      <div>{userExists ? "A user with this email already exists!" : ""}</div>
+      <button
+        onClick={() => {
+          push("/");
+        }}
+      >
+        Back
+      </button>
+    </div>
+  );
+}
