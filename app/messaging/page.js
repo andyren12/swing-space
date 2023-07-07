ecc1d2431a64f43987d596ae8c10153f16222b73
Bach Ngo <ngo.b48@gmail.com>
2023-07-07 02:12:32 -0400

    added client-side socket, displaying chats when sent, chat styling, TODO cmts

--- app/messaging/page.js
+++ app/messaging/page.js
@@ -8,120 +8,119 @@ import pfp from "../../public/person.png"
 import { Avatar } from "@chakra-ui/react";
 import axios from "axios";
 
-const socket = io(process.env.BASE_URL, { autoConnect: false });
-
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
+//TODO: message notifs and connection status
 
 export default function Message() {
-  const [message, setMessage] = useState("");
-  const [username, setUsername] = useState("");
-  const [allMessages, setAllMessages] = useState([]);
-  const [allChats, setAllChats] = useState([])
-  const [connectedUsers, setConnectedUsers] = useState([])
-  const [selectedUser, setSelectedUser] = useState([])
-  const { data: session } = useSession();
-  const id = session.user._id.toString();
-  const role = session.user.role.toString();
+    const [message, setMessage] = useState("");
+    const [username, setUsername] = useState("");
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [selectedMsgHistory, setSelectedMsgHistory] = useState(null)
+    const [allChats, setAllChats] = useState([])
+    const [connectedUsers, setConnectedUsers] = useState([])
+    //have an array state that holds if connected users are actually connected to the server(online)
+    const [selectedUser, setSelectedUser] = useState([])
+    const { data: session } = useSession();
+    const user = (session) ? session.user : "";
+    const socket = io(process.env.BASE_URL, { 
+        autoConnect: false, 
+        auth: { 
+            email: user.email,
+            username: user.firstName + " " + user.lastName,
+        },
+        id: user._id
+    });
+
+    socket.onAny((event, ...args) => {
+        console.log(event, args);
+    });
+
+    socket.on("private message", ({content, from, fromUser}) => {
+        if(from === user._id) return;
+        const newMsg = {
+            sender: fromUser,
+            content: content,
+            send_date: Date.now()
+        }
+        setSelectedMsgHistory([...selectedMsgHistory, newMsg])
+    })
+
+    useEffect(() => {
+        const fetchSubscriptions = async () => {
+            try {
+                const r = await fetch(`${process.env.SERVER_URI}subscribe/getSubscriptions/${user._id.toString()}`)
+                const response = await r.json();
+                setAllChats((prev) => response)
+            } catch(e) {
+                console.error(e)
+            }
+        };
+        const fetchSubscribers = async () => {
+            try {
+                const r = await fetch(`${process.env.SERVER_URI}subscribe/getSubscribers/${user._id.toString()}`)
+                const response = await r.json();
+                setAllChats((prev) => response)
+            } catch(e) {
+                console.error(e)
+            }
+        };
+        if (user.role === "student") fetchSubscriptions();
+        else fetchSubscribers();
+    },[user])
+
+    useEffect(() => {
+        const getUser = async() => {
+            console.log(allChats.length)
+            let newArr = []
+            for await (const chats of allChats.map(chat => fetch(`${process.env.SERVER_URI}api/account/${(user.role === "student") ? chat.coachId : chat.studentId}`))) {
+                const u = await chats.json()
+                console.log(u)
+                newArr = [...newArr, u]
+            }
+            setConnectedUsers(prev => newArr)
+        }
+        getUser();
+    }, [allChats])
 
 
-  useEffect(() => {
-    const fetchSubscriptions = async () => {
+    const handleSubmit = async(e) => {
+        if(message == "") return;
+        e.preventDefault();
+
+        setSelectedMsgHistory([...selectedMsgHistory, {
+            sender: user.firstName + " " + user.lastName,
+            content: message,
+            send_date: Date.now()
+        }])
+
         try {
-            const r = await fetch(`${process.env.SERVER_URI}subscribe/getSubscriptions/${id.toString()}`)
-            const response = await r.json();
-            setAllChats((prev) => response)
-        } catch(e) {
-            console.error(e)
+            const addMsg = await fetch(`${process.env.SERVER_URI}subscribe/addMessage`, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json",
+                    },
+                body: JSON.stringify({
+                    sender: user.firstName + " " + user.lastName,
+                    content: message,
+                    id: selectedChat._id
+                })
+            })
+            console.log(addMsg.data)
+        } catch(err) {
+            console.log(err)
         }
-    };
-    const fetchSubscribers = async () => {
-        try {
-            const r = await fetch(`${process.env.SERVER_URI}subscribe/getSubscribers/${id.toString()}`)
-            const response = await r.json();
-            setAllChats((prev) => response)
-        } catch(e) {
-            console.error(e)
-        }
-    };
-    if (role === "student") fetchSubscriptions();
-    else fetchSubscribers();
-  },[role, id])
 
-  useEffect(() => {
-    const getUser = async() => {
-        console.log(allChats.length)
-        let newArr = []
-        for await (const chats of allChats.map(chat => fetch(`${process.env.SERVER_URI}api/account/${chat.coachId}`))) {
-            const u = await chats.json()
-            console.log(u)
-            newArr = [...newArr, u]
-        }
-        setConnectedUsers(prev => newArr)
-    }
-    getUser();
-  }, [allChats])
-
-  useEffect(() => {
-    // socket.auth = { id }
-    // socket.id = id
-    // socket.connect();
-
-    // socket.on("users", (users) => {
-    //     users.forEach((user) => {
-    //       user.self = user.userID === socket.id;
-    //     });
-    //     // put the current user first, and then sort by username
-    //     this.users = users.sort((a, b) => {
-    //       if (a.self) return -1;
-    //       if (b.self) return 1;
-    //       if (a.username < b.username) return -1;
-    //       return a.username > b.username ? 1 : 0;
-    //     });
-    //   });
-
-
-    // socket.on("private message", ({ content, from }) => {
-    //     for (let i = 0; i < allChats.length; i++) {
-    //       const user = this.connected[i];
-    //       if (user.userID === from) {
-    //         user.messages.push({
-    //           content,
-    //           fromSelf: false,
-    //         });
-    //         if (user !== this.selectedUser) {
-    //           user.hasNewMessages = true;
-    //         }
-    //         break;
-    //       }
-    //     }
-    // });
-
-
-
-  },[])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (selectedUser) {
         socket.emit("private message", {
-          content,
-          to: selectedUser.id,
+            content: message,
+            to: (user.role === "student") ? selectedChat.coachId : selectedChat.studentId,
         });
-        // selectedUser.messages.push({
-        //   content,
-        //   fromSelf: true,
-        // });
+
+        setMessage("");
     }
 
-    setMessage("");
-  }
-
-  const handleChangeChat = (user) => {
-    setSelectedUser(user)
-  }
+    const handleChangeChat = (i) => {
+        setSelectedChat(allChats[i])
+        setSelectedMsgHistory(allChats[i].message_history)
+    }
 
     const users = [
         {
@@ -143,10 +142,10 @@ export default function Message() {
 
   return (
     <div
-    className="h-screen w-screen overflow-hidden"
+    className="pt-16 h-screen w-screen overflow-hidden"
     >
         <div
-        className="absolute top-16 w-full h-full flex flex-row overflow-hidden border-t-2 border-black"
+        className="w-full h-full flex flex-row overflow-hidden border-t-2 border-black"
         >
             <div
             className="h-full w-1/4 bg-white overflow-auto border-x-2 border-black"
@@ -158,76 +157,81 @@ export default function Message() {
                 </div>
                 {/* {console.log(connectedUsers.length)} */}
                 {(connectedUsers.length > 0) ? (connectedUsers.map((user, idx) => (
-                    <div
+                    <button
                     className={"h-20 w-full p-2 hover:bg-slate-100 text-lg font-bold border-black flex items-center"}
                     key={idx}
-                    onClick={() => handleChangeChat(user)}
+                    onClick={() => handleChangeChat(idx)}
                     >
                         <Avatar src={pfp} size="sm" margin={2}/>
                         {user.firstName + " " + user.lastName}
+                    </button>
+                ))): (
+                    <div
+                    className="h-20 w-full p-2 text-xl font-semibold text-center text-red-500"
+                    >
+                        No Messages
                     </div>
-                ))): null}
+                )}
             </div>
 
 
             <div
-            className="h-full w-3/4 bg-slate-50 flex-col-reverse flex"
+            className="h-full w-3/4 bg-slate-400 flex-col-reverse flex"
             >
-                <div
-                className="w-full flex-row pb-20"
+                {selectedChat && (<div
+                className="w-full flex-row"
                 >
                     <input
                         name="message"
                         placeholder="enter your message"
                         value={message}
+                        onKeyDown={(e) => {
+                            if(e.key === "Enter") {
+                                e.preventDefault();
+                                handleSubmit(e);
+                            }
+                        }}
                         onChange={(e) => setMessage(e.target.value)}
                         className="border-2 border-slate-400 p-2 ml-16 mb-8 rounded-lg w-10/12"
                     />
                     <button
-                    className="bg-black text-white rounded-lg p-2 m-2 w-16"
+                    className="bg-black text-white rounded-lg p-2 m-2 w-20 font-bold"
                     onClick={handleSubmit}
                     >
                         Send
                     </button>
-                </div>
+                </div>)}
                 <div
-                    id="msg-hist"
-                    className="mx-16 my-6 h-3/4 p-2"
-                    >
-                        Message History goes here
+                id="msg-hist"
+                className="mx-8 my-6 h-5/6 p-2 bg-slate-100 overflow-auto text-5xl font-bold"
+                >
+                    {(selectedMsgHistory) ? (
+                        selectedMsgHistory.map((msg, idx) => (
+                            <div
+                            key={idx}
+                            className="w-full px-4"
+                            >   
+                                {((idx - 1) < 0 || selectedMsgHistory[idx-1].sender != msg.sender) ? 
+                                (<div
+                                className="font-extrabold text-xl mt-4"
+                                >
+                                    {msg.sender}
+                                </div>) : 
+                                null}
+                                <div
+                                className="px-4 font-semibold text-base mt-1"
+                                >
+                                    {msg.content}
+                                </div>
+                            </div>
+                        ))
+                    ) :
+                    "You must select a chat"}
                 </div>
             </div>
         </div>
     </div>
-    // <div>
-    //   <h1>Chat app</h1>
-    //   <h1>Enter a username</h1>
-
-    //   <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username" />
-
-    //   <br />
-    //   <br />
-
-    //   <div>
-    //     {allMessages.map(({ username, message }, index) => (
-    //       <div key={index}>
-    //         {username}: {message}
-    //       </div>
-    //     ))}
-
-    //     <br />
-
-    //     <form onSubmit={handleSubmit}>
-    //       <input
-    //         name="message"
-    //         placeholder="enter your message"
-    //         value={message}
-    //         onChange={(e) => setMessage(e.target.value)}
-    //         autoComplete={"off"}
-    //       />
-    //     </form>
-    //   </div>
-    // </div>
+    
   );
 };
 
